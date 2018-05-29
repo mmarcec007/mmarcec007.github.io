ea9be6337ba165317de0639174a5aef1ee08ff6a
mmarcec007 <mark.marcec@gmail.com>
2018-05-29 17:52:14 +0200

    [TASK] integrate new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ImageGallery from './shutterstock/components/ImageGallery.js'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload on develop branch.
         </p>
+        <ImageGallery/>
       </div>
     );
   }
