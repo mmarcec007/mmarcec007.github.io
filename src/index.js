a87d8c100e6646d8613c6424c71c4eeca852418e
mmarcec007 <mark.marcec@gmail.com>
2018-06-01 17:02:11 +0200

    [TASK] integrate bootstrap 4 and baguettebox gallery

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'reactstrap/dist/reactstrap.min'
+import 'baguettebox.js'
+import 'baguettebox.js/dist/baguetteBox.min.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
